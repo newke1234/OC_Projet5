cb151b4c687d49b85a497ac3a95d85cbebebbde5
Nicolas <contact@nicolasrichelet.com>
2023-12-27 16:53:42 +0100

    Enlevé le tabindex="1" pour l'accessibilité

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -200,7 +200,7 @@
     createLightBox(gallery, lightboxId, navigation) {
       gallery.append(`<div class="modal fade" id="${
         lightboxId ? lightboxId : "galleryLightbox"
-      }" tabindex="1" role="dialog" aria-hidden="true">
+      }" role="dialog" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
                         <div class="modal-body">
